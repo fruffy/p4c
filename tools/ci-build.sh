09dabf14b917491218d00b95f310577ab829a3c7
fruffy <fruffy@nyu.edu>
2022-02-12 17:26:49 -0500

    Permission check.

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -5,6 +5,9 @@
 set -e  # Exit on error.
 set -x  # Make command execution verbose
 
+# Check whether we can even write to ccache.
+touch .ccache/example_file
+
 export P4C_DEPS="bison \
              build-essential \
              cmake \
