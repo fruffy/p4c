55e843797d4b4304194215b0fad33cdd6ba8d2fb
fruffy <fruffy@nyu.edu>
2024-07-10 13:52:50 -0400

    Run typechecking after front and mid end.
    Signed-off-by: fruffy <fruffy@nyu.edu>

--- backends/p4tools/common/compiler/compiler_target.cpp
+++ backends/p4tools/common/compiler/compiler_target.cpp
@@ -92,6 +92,10 @@ const IR::P4Program *CompilerTarget::runFrontend(const CompilerOptions &options,
     auto frontEnd = mkFrontEnd();
     frontEnd.addDebugHook(options.getDebugHook());
     program = frontEnd.run(options, program);
+    P4::ReferenceMap refMap;
+    P4::TypeMap typeMap;
+    // Perform a last round of type checking.
+    program = program->apply(P4::TypeChecking(&refMap, &typeMap, true));
     if ((program == nullptr) || ::errorCount() > 0) {
         return nullptr;
     }
@@ -111,7 +115,12 @@ const IR::P4Program *CompilerTarget::runMidEnd(const CompilerOptions &options,
                                                const IR::P4Program *program) const {
     auto midEnd = mkMidEnd(options);
     midEnd.addDebugHook(options.getDebugHook(), true);
-    return program->apply(midEnd);
+    program = program->apply(midEnd);
+    P4::ReferenceMap refMap;
+    P4::TypeMap typeMap;
+    // Perform a last round of type checking.
+    program = program->apply(P4::TypeChecking(midEnd.getRefMap(), midEnd.getTypeMap(), true));
+    return program;
 }
 
 CompilerTarget::CompilerTarget(std::string_view toolName, const std::string &deviceName,
